d083a2b78a7fa092222077a94b942bdf4d722e08
Emily Bache <emily@bacheconsulting.com>
2022-08-09 11:45:56 +0000

    reset to starting position

--- birds_insects/test/test_rig.py
+++ birds_insects/test/test_rig.py
@@ -7,7 +7,7 @@ import load_observations
 def main():
     testdbname = "ttdb_" + str(os.getpid())  # some temporary name not to clash with other tests
 
-    # Switch dbtext.MSSQL_DBText for dbtext.Sqlite3_DBText or dbtext.MySQL_DBText as you prefer
+    # Switch between dbtext.MSSQL_DBText or dbtext.Sqlite3_DBText or dbtext.MySQL_DBText as you prefer
     with dbtext.MSSQL_DBText(testdbname) as db:
         # Arrange
         db.create(sqlfile="empty_db.sql")
