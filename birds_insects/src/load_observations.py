9c0620e78ea901bacffcc5998365b7c3158a6b1b
Emily Bache <emily@bacheconsulting.com>
2022-08-22 14:38:36 +0000

    added exercises in README

--- birds_insects/src/load_observations.py
+++ birds_insects/src/load_observations.py
@@ -11,40 +11,46 @@ from model import Wildlife, Observation
 
 
 def create_sqlalchemy_engine(connection_string):
-	connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
 
-	engine = create_engine(connection_url)
-	return engine
+    engine = create_engine(connection_url)
+    return engine
+
+
+def add_observation(date, result, session):
+    isodate = datetime.date.fromisoformat(date)
+    observation = Observation(date=isodate, wildlife=result)
+    session.add(observation)
 
 
 def main(connection_string):
-	print("Loading observations")
-	engine = create_sqlalchemy_engine(connection_string)
-	with Session(engine, future=True) as session:
-		with open("observations.csv") as f:
-			reader = csv.DictReader(f, delimiter=",")
-			for row in reader:
-				print(f"loading {row}")
-				date = row['date']
-				type = row['type']
-				name = row['name']
-				stmt = select(Wildlife).filter_by(name=name, type=type)
-				results = session.execute(stmt).scalars().all()
-				if not results:
-					wildlife = Wildlife(name=name, type=type)
-					session.add(wildlife)
-					results = [wildlife]
-				for result in results:
-					isodate = datetime.date.fromisoformat(date)
-					observation = Observation(date=isodate, wildlife=result)
-					session.add(observation)
-		session.commit()
+    print("Loading observations")
+    engine = create_sqlalchemy_engine(connection_string)
+    with Session(engine, future=True) as session:
+        with open("observations.csv") as f:
+            reader = csv.DictReader(f, delimiter=",")
+            for row in reader:
+                print(f"loading {row}")
+                date = row['date']
+                type = row['type']
+                name = row['name']
+                stmt = select(Wildlife).filter_by(name=name, type=type)
+                results = session.execute(stmt).scalars().all()
+                if not results:
+                    wildlife = Wildlife(name=name, type=type)
+                    session.add(wildlife)
+                    results = [wildlife]
+                for result in results:
+                    add_observation(date, result, session)
+        session.commit()
 
 
 if __name__ == "__main__":
-	import sys
-	if len(sys.argv) < 2:
-		print("you must supply an argument - the name of the localdb database to connect to")
-		sys.exit(-1)
-	dbname = sys.argv[1]
-	main("DRIVER={ODBC Driver 17 for SQL Server};SERVER=(localdb)\MSSQLLocalDB;Integrated Security=true;DATABASE={dbname};")
+    import sys
+
+    if len(sys.argv) < 2:
+        print("you must supply an argument - the name of the localdb database to connect to")
+        sys.exit(-1)
+    dbname = sys.argv[1]
+    main(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER=(localdb)\MSSQLLocalDB;Integrated Security=true;DATABASE={dbname};")
